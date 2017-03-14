8dc1cee16564cc038f9f3b113108d3e3f80ac89a
Jerry Mesimaki <jerry.mesimaki@cs.helsinki.fi>
2017-03-14 14:20:24 +0200

    tunings 3

--- config.js
+++ config.js
@@ -15,10 +15,10 @@ module.exports = {
     knex: {
       client: 'postgres',
       connection: {
-        host : '',
-        user : '',
-        password : '',
-        database : ''
+        host : process.env.DB_HOST || '',
+        user : process.env.DB_USER || '',
+        password : process.env.DB_PASSWORD || '',
+        database : process.env.DB_DATABASE || ''
       }
     }
   }
